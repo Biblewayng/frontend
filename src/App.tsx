71e79acec0b36cb70d8d0947aabeb1af07ff7c36
Adelodun Peter <adelodunpeter69@gmail.com>
2026-04-20 14:04:03 +0100

    fix: remove duplicate react import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { lazy, Suspense } from 'react'
-import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import { ProtectedRoute } from './components/ProtectedRoute'
