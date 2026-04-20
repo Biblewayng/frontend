21002b76b982c4275b4e4ca4a8a0307b9d98ed31
Adelodun Peter <adelodunpeter69@gmail.com>
2026-04-20 12:40:47 +0100

    fix: remove duplicate Link import in LandingPage

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { Link } from "react-router-dom";
 import PublicNavbar from "@/components/layout/PublicNavbar";
