9f675c9df400431c745854e8a5733a4363131098
stanmart1 <scaleitpro@gmail.com>
2026-04-26 22:48:07 +0100

    update dev/preview server port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,11 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 3000,
   },
   preview: {
     host: '0.0.0.0',
-    port: 4173,
+    port: 3000,
     allowedHosts: 'all',
   },
 })
